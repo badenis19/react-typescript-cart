c771aa97a824230c76690907ad86e1122b5eb8b3
Bruno <warren@Warrens-MacBook-Pro.local>
2023-10-20 10:36:57 +0100

    page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./components/Home";
+import Store from "./components/Store";
+import About from "./components/About";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
+};
 
-export default App
+export default App;
