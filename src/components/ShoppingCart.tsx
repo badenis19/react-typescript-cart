4e38b6c7e6ceb0b0b0f9d77e7713b1f640f12008
Bruno <warren@Warrens-MacBook-Pro.local>
2023-10-22 16:15:14 +0100

    Offcanvas slider design and functions working

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,20 +1,38 @@
-import { Offcanvas } from "react-bootstrap"
-import { useShoppingCart } from "../context/ShoppingCartContext"
-
+import { Offcanvas, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import CartItem from "./CartItem";
+import { formatCurrency } from "../utilities/FormatCurrency";
+import storeItems from "../data/items.json";
 
 type ShoppingCartProps = {
-    isOpen: boolean
-}
+  isOpen: boolean;
+};
 
-const ShoppingCart = ({isOpen} : ShoppingCartProps) => {
-    const { closeCart } = useShoppingCart()
+const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
+  const { closeCart, cartItems } = useShoppingCart();
   return (
-   <Offcanvas show={isOpen} onHide={closeCart} placement="end">
-        <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Cart</Offcanvas.Title>
-        </Offcanvas.Header>
-   </Offcanvas>
-  )
-}
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+        </Stack>
+        <div className="ms-auto fw-bold fs-5">
+          Total{" "}
+          {formatCurrency(
+            cartItems.reduce((total, cartItem) => {
+              const item = storeItems.find((i) => i.id === cartItem.id);
+              return total + (item?.price || 0) * cartItem.quantity;
+            }, 0)
+          )}
+        </div>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
+};
 
-export default ShoppingCart
+export default ShoppingCart;
